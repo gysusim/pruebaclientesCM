e9e67689e67243bb11eef8ab4f63a498be838d99
gysusim <145064063+gysusim@users.noreply.github.com>
2024-12-18 23:23:39 +0000

    Update streamlit_app.py
    Added a few lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,7 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "I want to try this out. Specially now with the GitHub desktop version."
+)
+st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
